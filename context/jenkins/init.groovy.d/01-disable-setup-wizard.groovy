0bd5c25e23c58f6b49afe5e967e7d3ab034d61eb
pooyanazad <pooyan.azadparvar@gmail.com>
2025-12-14 14:12:20 +0100

    Add Jenkins init script: disable-setup-wizard

---
+++ context/jenkins/init.groovy.d/01-disable-setup-wizard.groovy
@@ -0,0 +1,10 @@
+import jenkins.model.*
+import hudson.util.*;
+import jenkins.install.*;
+
+def instance = Jenkins.getInstance()
+
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+instance.save()
+
+println "Jenkins setup wizard disabled"
